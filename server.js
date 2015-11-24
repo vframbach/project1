87454e95cf0627c589fe8a50243873b349f51306
vframbach <vframbach@gmail.com>
2015-11-24 14:31:43 -0800

    create files

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	hbs = require('hbs'),
+	mongoose = require('mongoose');
+
+
+
+
+
+// listen on port 3000
+http.listen(3000, function() {
+	console.log('server started');
+});

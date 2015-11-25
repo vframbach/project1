19c60097ca9828425a4f28a1a5e1f59d3c4856a9
vframbach <vframbach@gmail.com>
2015-11-24 19:38:04 -0800

    got new api key

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var express = require('express'),
 	// require and load dotenv
 	require('dotenv').load();
 
+var meetupKey = process.env.MEETUP_API_KEY;
+
 
 // configure body-parser (for form data)
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,19 +27,21 @@ app.set('view engine', 'hbs');
 // API routes
 app.get('/', function (req, res) {
 
-	res.render(index);
+	res.render('index');
 });
 
 
 
 
 app.get('/takeahike', function(req, res) {
-	request.get('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&zip=94111&topic=hiking,%20hike%20hikes&radius=smart&key=3a4c3823204a7f3c72346e673bf6a1d', function(error, response, body) {
+	request.get('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&zip=94111&topic=hiking,%20hike%20hikes&radius=smart&key='+ meetupKey, function(error, response, body) {
+		console.log(error);	
 		if(!error && response.statusCode == 200) {
 			console.log(body);
 		}
 	});
 });
+
 
 
 // listen on port 3000

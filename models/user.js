bbf3b9eb77ae03bdaddca826e5813d6322fd651f
vframbach <vframbach@gmail.com>
2015-11-29 22:55:12 -0800

    set up passport, sign up, log in and logout

--- models/user.js
+++ models/user.js
@@ -1,10 +1,15 @@
 var mongoose = require('mongoose');
 	Schema = mongoose.Schema;
+	passportLocalMongoose = require('passport-local-mongoose');
 
 var UserSchema = new Schema({
-	task: String,
-	description: String
+	username: String,
+	password: String
+
 });
+
+
+UserSchema.plugin(passportLocalMongoose);
 
 var User = mongoose.model('User', UserSchema);
 
--- server.js
+++ server.js
@@ -5,7 +5,13 @@ var express = require('express'),
 	bodyParser = require('body-parser'),
 	hbs = require('hbs'),
 	mongoose = require('mongoose'),
-	request = require('request');
+	request = require('request'),
+
+	// auth
+	cookieParser = require('cookie-parser'),
+	session = require('express-session'),
+	passport = require('passport'),
+	LocalStrategy = require('passport-local').Strategy;
 
 	// require and load dotenv
 	require('dotenv').load();
@@ -20,6 +26,20 @@ mongoose.connect('mongodb://localhost/project1');
 var User = require('./models/user');
 
 
+// tells express to use auth middleware
+app.use(cookieParser());
+app.use(session({
+	secret: 'supersecretkey',
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport config, allow users to sign up, log in and out
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // configure body-parser (for form data)
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -44,7 +64,7 @@ app.get('/', function (req, res) {
 app.get('/takeahike', function(req, res) {
 	var zipcode = req.query.zipcode;
 	request.get('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&zip='+ zipcode +'&topic=hiking,%20hike%20hikes&radius=smart&key='+ meetupKey, function(error, response, body) {
-		console.log(error);	
+		console.log('meetup response error', error);	
 		if(!error && response.statusCode == 200) {
 			var info = JSON.parse(body);
 			res.render("map", { events: info.results });
@@ -52,6 +72,43 @@ app.get('/takeahike', function(req, res) {
 	});
 });
 
+
+// auth routes
+
+// show signup view
+app.get('/signup', function (req, res) {
+	res.render('signup');
+});
+
+// show login view
+app.get('/login', function (req, res) {
+	res.render('login');
+});
+
+
+
+// sign up new user, then log them in
+// hashes and salts passport, saves new user to db
+app.post('/signup', function (req, res) {
+	User.register(new User({ username: req.body.username}), req.body.password,
+		function (err, newUser) {
+			passport.authenticate('local')(req, res, function() {
+				res.send('signed up!');
+			});
+		}
+	);
+});
+
+// log in user
+app.post('/login', passport.authenticate('local'), function (req, res) {
+	res.send('logged in!');
+});
+
+// log out user
+app.get('/logout', function (req, res) {
+	req.logout();
+	res.redirect('/');
+});
 
 
 // listen on port 3000
